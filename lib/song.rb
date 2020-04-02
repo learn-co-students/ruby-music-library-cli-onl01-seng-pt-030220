0588cdd34835a74b948378d06158f6b716ca57ef
Clannis <Dodds1990@gmail.com>
2020-04-02 05:34:25 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,8 @@ require 'pry'
 
 class Song
 
+    extend Concerns::Findable
+
     attr_accessor :name
     attr_reader :artist, :genre
 
@@ -45,18 +47,6 @@ class Song
             if !genre.songs.include?(self)
                 genre.songs << self
             end
-        end
-    end
-    
-    def self.find_by_name(name)
-        self.all.find { |song| song.name == name}
-    end
-
-    def self.find_or_create_by_name(name)
-        if self.find_by_name(name) == nil
-            self.create(name)
-        else
-            self.find_by_name(name)
         end
     end
 
