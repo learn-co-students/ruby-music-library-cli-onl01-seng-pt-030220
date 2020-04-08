459a14c841decbaac1910acc2615269bdeaaed14
ariel murphy <arielladders@gmail.com>
2020-04-08 15:45:34 +0000

    Done.

---
+++ lib/Artist.rb
@@ -0,0 +1,43 @@
+require_relative './concerns/findable.rb'
+
+class Artist
+
+ extend Concerns::Findable
+
+  @@all = []
+
+ def self.all
+  @@all
+ end
+
+ def self.destroy_all
+  @@all.clear
+ end
+
+ def self.create(artist_name)
+  new_artist = Artist.new(artist_name)
+  new_artist.save
+  new_artist
+ end
+
+ attr_accessor :name, :songs
+
+ def initialize(name)
+  @name = name
+  @songs = []
+ end
+
+ def save
+  @@all << self
+ end
+
+ def add_song(song)
+  song.artist == self ? song.artist : song.artist = self
+  self.songs << song unless self.songs.include?(song)
+ end
+
+ def genres
+  self.songs.map {|song| song.genre}.uniq
+ end
+    
+end
