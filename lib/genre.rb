3d96c6c5cdbd7a1e35fc6f7134541e3c617e08b4
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-04-09 00:11:20 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,36 @@
+class Genre
+
+  attr_accessor :name, :songs
+  extend Concerns::Findable
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+  end
+
+  def artists
+    self.songs.collect do |artists|
+      artists.artist
+    end.uniq
+  end
+end
