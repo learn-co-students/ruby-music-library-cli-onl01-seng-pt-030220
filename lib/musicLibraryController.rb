73730150d2de7ca3cee35f1ff5178e1e294f47c7
alelee93 <leevargasale@gmail.com>
2020-04-11 17:45:54 -0500

    Done.

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -10,19 +10,20 @@
     end
 
     def call
-        puts "Welcome to your music library!"
-        puts "To list all of your songs, enter 'list songs'."
-        puts "To list all of the artists in your library, enter 'list artists'."
-        puts "To list all of the genres in your library, enter 'list genres'."
-        puts "To list all of the songs by a particular artist, enter 'list artist'."
-        puts "To list all of the songs of a particular genre, enter 'list genre'."
-        puts "To play a song, enter 'play song'."
-        puts "To quit, type 'exit'."
-        puts "What would you like to do?"
+        user_input = ''
+        while user_input != 'exit'
+            puts "Welcome to your music library!"
+            puts "To list all of your songs, enter 'list songs'."
+            puts "To list all of the artists in your library, enter 'list artists'."
+            puts "To list all of the genres in your library, enter 'list genres'."
+            puts "To list all of the songs by a particular artist, enter 'list artist'."
+            puts "To list all of the songs of a particular genre, enter 'list genre'."
+            puts "To play a song, enter 'play song'."
+            puts "To quit, type 'exit'."
+            puts "What would you like to do?"
 
-        user_input = gets.chomp 
+            user_input = gets.chomp 
         
-        if user_input != 'exit'
             case user_input
                 when 'list songs'
                     list_songs
@@ -38,8 +39,8 @@
                     play_song
             end
         end
-        
     end
+    
 
     def list_songs
         my_songs = Song.all.sort { |song1, song2| song1.name <=> song2.name} 
