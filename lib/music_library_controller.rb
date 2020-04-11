1ddf3e0861721fd1cff2ebd37a6f284e8f42ea2f
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-04-11 14:24:42 -0700

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -8,31 +8,40 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    user_input = gets.chomp
-    case user_input
-    when 'list songs'
-      list_songs
-    when 'list artists'
-      list_artists
-    when 'list genres'
-      list_genres
-    when 'list artist'
-      list_songs_by_artist
-    when 'list genre'
-      list_songs_by_genre
-    when 'play song'
-      play_song
-    end
-    user_input == 'exit' || call # if user does not type 'exit', loop back and ask again
+
+    user_input = ''
+
+    while user_input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+
+      user_input = gets.chomp
+
+        case user_input
+        when 'list songs'
+          list_songs
+        when 'list artists'
+          list_artists
+        when 'list genres'
+          list_genres
+        when 'list artist'
+          list_songs_by_artist
+        when 'list genre'
+          list_songs_by_genre
+        when 'play song'
+          play_song
+        else 
+          'Request not valid, please try again.'
+        end
+      end
+    # user_input == 'exit' || call
   end
 
   def list_songs
@@ -80,13 +89,9 @@ class MusicLibraryController
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
-
+  
 end
 
-
-
-
-
-
+# MusicLibraryController.new.call
 # lib = MusicLibraryController.new(filepath)
 # lib.list_songs
