32634b10be890bf5ae934a8ddf07ff851842d11f
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-04-11 14:38:10 -0700

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -77,19 +77,19 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
+    
     input = gets.chomp.to_i
 
-    if (1..Song.all.length).include?(input)
+    if input.between?(1,Song.all.length) #(1..Song.all.length).include?(input) 
       song = Song.all.sort_by{ |song| song.name }[input - 1]
     end
     
-    if song
+    if song 
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
   
 end
-
 # MusicLibraryController.new.call
 # lib = MusicLibraryController.new(filepath)
 # lib.list_songs
